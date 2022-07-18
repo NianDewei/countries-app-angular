2a683dec7e5614297fc1da34bd9e22d2eae8848e
Rolando Oc <rolandowc@hotmail.com>
2022-07-18 17:10:39 -0500

    20 | Search by category v2

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -4,24 +4,38 @@ import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-region',
-  templateUrl: './by-region.component.html',
-  styles: [],
+  templateUrl: './by-region.component.html'
 })
 export class ByRegionComponent implements OnInit {
   term: string = '';
   error: boolean = false;
+  regions: string[] = ['Asia', 'Europe', 'America', 'South America', 'Oceania'];
+  activeRegion: string = '';
   countries: Country[] = [];
+
 
   constructor(private countryService: CountryService) {}
 
   ngOnInit(): void {}
 
   // methods
-  search(term: string): void {
+  getActiveRegion(region: string) {
+    if (region === this.activeRegion) {
+      return;
+    }
+    this.activeRegion = region;
+    this.search(region);
+  }
+
+  getClassActive(region: string): string {
+    return region === this.activeRegion
+      ? 'btn btn-primary m-1'
+      : 'btn btn-outline-primary m-1';
+  }
+
+  private search(term: string): void {
     this.error = false;
     this.term = term;
-
-    console.log(term);
 
     this.countryService.searchByRegion(term).subscribe({
       // recommended way
